d67454019920ac008bb41d849f91459d5810fba3
Paul Robertson <paul.robertson@place4waste.com>
2020-05-16 09:59:41 +0100

    Update life.hs

--- life.hs
+++ life.hs
@@ -1,20 +1,52 @@
+module Main(main) where
+
 import Graphics.Gloss
 import Data.List
 import System.IO
 
+-- helper functions for 2D arrays [[Int]]
+-- map, zip, and zipWith(+)
+-- 
+gridMap :: (a -> b) -> [[a]] -> [[b]]
 gridMap = map.map
+
+gridZip :: [[a]] -> [[b]] -> [[(a, b)]]
 gridZip = zipWith zip
+
+gzwp :: Num c => [[c]] -> [[c]] -> [[c]]
 gzwp = zipWith (zipWith (+))
+
+-- the rules of life
+-- 
 live = 1
 die = 0
 
-rulesOfLife (1, y) = if y==3 || y==4 then live else die
-rulesOfLife (0, 3)    = live
-rulesOfLife (_, _)    = die
+-- (x, y)
+-- x = 0 - uninhabited (dead) cell
+-- x = 1 - inhabited (live) cell
+-- y = number of neighbours *including* the cell itself
+-- 
+rulesOfLife :: (Int, Int) -> Int
+rulesOfLife (1, 3) = live
+rulesOfLife (1, 4) = live
+rulesOfLife (0, 3) = live
+rulesOfLife (_, _) = die
 
+-- The game takes place in a square grid of size l
+--
 l = 100 :: Int
 l2 = 50
 
+-- Spawn the next generation
+-- map each cell to its neighbour count
+-- zip with the original array
+-- map the life rules over the result
+-- 
+-- 0 1 1      2 3 1      (0,2) (1,3) (1,1)      0 1 0
+-- 1 0 0  ->  3 4 3  ->  (1,3) (0,4) (0,3)  ->  1 0 1
+-- 0 1 0      2 2 1      (0,2) (1,2) (0,1)      0 0 0
+-- 
+generate :: [[Int]] -> [[Int]]
 generate x = gridMap rulesOfLife (gridZip x u)
              where y = gzwp x (zs:x)
                    w = transpose $ gzwp y ((tail x)++[zs])
@@ -22,10 +54,14 @@ generate x = gridMap rulesOfLife (gridZip x u)
                    u = transpose $ gzwp v ((tail w)++[zs])
                    zs = replicate l 0
 
-m (x, y) = (x, fromIntegral (y `mod` l), fromIntegral (y `div` l))
-
+-- Draw the array of cells
+-- First flatten the array and convert each cell value to a triple
+-- (val, x, y) where (x,y) are the coordinates of the cell
+-- 
+drawer :: [[Int]] -> Picture
 drawer x = pictures $ map d z
            where y = zip (concat x) [0..]
+                 m (x, y) = (x, fromIntegral (y `mod` l), fromIntegral (y `div` l))
                  z = map m y
                  d (0,col,row) = translate ((col-l2)*10) ((l2-row)*10) $ color white $ rectangleSolid 8 8
                  d (_,col,row) = translate ((col-l2)*10) ((l2-row)*10) $ color black $ rectangleSolid 8 8
@@ -38,7 +74,7 @@ ext x = (replicate len1 0) ++ x ++ (replicate len2 0)
         where len = length x
               len1 = (l - len) `div` 2
               len2 = l - len - len1
-
+main :: IO ()
 main = do
     handle <- openFile "seed.txt" ReadMode
     contents <- hGetContents handle
